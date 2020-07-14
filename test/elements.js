944fecd0da9d060ede53e3e8a40f3c68520c0667
irina08 <irina_ger@yahoo.com>
2020-07-14 00:53:05 -0700

    Added elements.js

---
+++ test/elements.js
@@ -0,0 +1,17 @@
+const selectorGen = require ('../data/selectors.json').general;
+const expectedGen = require ('./../data/expected.json').general;
+const selectorCnt = require ('../data/selectors.json').counter;
+const expectedCnt = require ('./../data/expected.json').counter;
+
+describe('Complex Counter App', function () {
+    describe('Getting to the page', function () {
+        it('TC-001 Page title is Complex Counter App', function () {
+            browser.url('');
+            const title = browser.getTitle();
+            expect(title).toEqual('Complex Counter App');
+        })
+    });
+
+
+
+});
